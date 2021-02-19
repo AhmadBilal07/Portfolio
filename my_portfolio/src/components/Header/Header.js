5c2da6b0d32cbb8f32dbc26ca99fc0a188910317
AhmadBilal07 <ahmadbilal07@live.com>
2021-02-19 20:00:54 +0000

    header updated

--- my_portfolio/src/components/Header/Header.js
+++ my_portfolio/src/components/Header/Header.js
@@ -3,31 +3,97 @@ import styled from "styled-components";
 const Styles = styled.div`
   header {
     display: none;
+    // z-index: 999;
+    // box-shadow: 5px 7px 15px -10px #888888;
+    background-color:white;
+    font-family:"arial";
+    text-transform:uppercase;
+    text-align:center;
     z-index: 999;
-    box-shadow: 5px 7px 15px -10px #888888
+    
+    line-height: 3;
   }
   header.scrolled {
-    background-color: white;
+    //background-color: white;
     position: fixed;
     top: 0;
     width: 100%;
     display: block;
   }
-  header a {
-    float: left;
-    color: #272341;
-    text-align: center;
-    padding: 5px 3.2vw;
-    text-decoration: none;
-    font-size:calc(5px + 1.1vw);
-    font-weight:normal;
-    display:inline-block;
-    box-shadow: 5px 7px 15px -12px #888888;
+#mainNav ul {
+  list-style: none;
+  margin:0;
+  padding-inline-start: 0px;
+  box-shadow: 0 1px 10px #777 !important;
+}
+#mainNav ul li {
+  display: inline-block;
+	display:inline;
+	margin: 20px;
+  @media only screen and (max-width: 600px) {
+    margin: 10px;
   }
-  header a:hover {
-    box-shadow: 5px 7px 15px 1px #888888;
-    cursor: pointer;
-  }
+}
+#mainNav ul li a {
+  padding-bottom: 5px;
+  text-decoration: none;
+  color:#0E538C;
+}
+#mainNav ul li a,
+#mainNav ul li a:after,
+#mainNav ul li a:before {
+  transition: all .5s;
+}
+#mainNav ul li a:hover {
+  color:#E34F25;
+}
+
+/* stroke */
+#mainNav.stroke ul li a
+{
+  position: relative;
+}
+#mainNav.stroke ul li a:after,
+#mainNav ul li a:after {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 0%;
+  content: '.';
+  color: transparent;
+  background: #0E538C;
+  height: 1px;
+}
+#mainNav.stroke ul li a:hover:after {
+  width: 100%;
+}
+
+#mainNav ul li a {
+  transition: all 1s;
+}
+
+#mainNav ul li a:after {
+  text-align: left;
+  content: '.';
+  margin: 0;
+  opacity: 0;
+}
+#mainNav ul li a:hover {
+  color: #02CBAD;
+  z-index: 1;
+}
+#mainNav ul li a:hover:after {
+  z-index: -10;
+  animation: fill 1s forwards;
+  opacity: 1;
+}
+#mainNav {
+  height:48px !important;
+
+}
+
 `;
 
 const Header = () => {
@@ -51,10 +117,22 @@ const Header = () => {
   return (
     <Styles>
       <header className={state.scrolled ? "scrolled" : ""}>
-        <a>About</a>
-        <a>Skills</a>
-        <a>Projects</a>
-        <a>Contact</a>
+        <nav class="stroke" id="mainNav">
+          <ul>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">Skills</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            </ul>
+        </nav>
       </header>
     </Styles>
   );
